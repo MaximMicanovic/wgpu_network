ce7e1d3f9600a879314a983fb0d9ebf7488a2918
Maxim Micanovic <maxim.micanovic@gmail.com>
2025-08-21 12:30:24 +0200

    fixed the vibe coded init which didnt work

--- src/network.rs
+++ src/network.rs
@@ -10,31 +10,30 @@ fn random_between_minus_one_and_one() -> f32 {
 }
 
 pub struct NeuralNetwork {
-    pub weights: Vec<f32>,
-    pub biases: Vec<f32>,
+    pub data: Vec<f32>,
     pub output: Vec<f32>,
 }
 
-
 impl NeuralNetwork {
     pub fn new(structure: &[usize]) -> Self {
-        let mut weights = Vec::new();
-        let mut biases = Vec::new();
+        let mut data = Vec::new();
         let mut output = Vec::new();
 
-        for i in 0..structure.len() - 1 {
-            for _ in 0..structure[i] * structure[i + 1] {
-                weights.push(random_between_minus_one_and_one());
+        for layer in 1..structure.len() {
+            
+            //Weights
+            for _perceptron in 1..structure[layer] {
+                for _weight in 0..structure[layer - 1] {
+                    data.push(random_between_minus_one_and_one());
+                }
             }
-            for _ in 0..structure[i + 1] {
-                biases.push(random_between_minus_one_and_one());
-            }
+            //Bias once per perceptron
+            data.push(random_between_minus_one_and_one())
         }
 
-        NeuralNetwork {
-            weights,
-            biases,
-            output,
+        return NeuralNetwork {
+            data: data,
+            output: output,
         }
     }
 }
